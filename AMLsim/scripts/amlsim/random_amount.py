9a74d5c70742a19985c6f4bcd053f3d217b6d3a3
johanos1 <40794255+johanos1@users.noreply.github.com>
2023-05-08 16:53:31 +0200

    Delete AMLsim directory

--- AMLsim/scripts/amlsim/random_amount.py
+++
@@ -1,9 +0,0 @@
-import random
-
-class RandomAmount:
-    def __init__(self, min, max):
-        self.min = min
-        self.max = max
-
-    def getAmount(self):
-        return random.uniform(self.min, self.max)
