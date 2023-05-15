44ba766b89a7f40736a0bc96c36da036872ab2c5
johanos1 <oshtman@gmail.com>
2023-05-15 11:45:40 +0200

    test

--- AMLsim/src/main/java/amlsim/AccountBehaviour.java
+++ AMLsim/src/main/java/amlsim/AccountBehaviour.java
@@ -110,6 +110,7 @@ public class AccountBehaviour {
             return newBank;
         } else {
             // If the bank is not changed, return the current bank
+            // test
             return currentBank;
         }
     }
