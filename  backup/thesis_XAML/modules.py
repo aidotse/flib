f96204750a6f27a9295f908ea82f4cfd1cc1ea9b
coffeeBeansz <tomas.pousette@gmail.com>
2024-02-27 12:41:42 +0000

    update modules

---
+++  backup/thesis_XAML/modules.py
@@ -0,0 +1,132 @@
+import torch
+from torch import nn
+from torch.nn import functional as F
+import torch_geometric
+from torch_geometric.nn import GCNConv, SAGEConv, GINEConv, GATConv, BatchNorm, Linear
+from torch_geometric.data import Data
+
+
+# (Written by Tomas & Agnes)
+class LogisticRegressor(torch.nn.Module):
+    def __init__(self, input_dim, output_dim):
+        super(LogisticRegressor, self).__init__()
+        self.linear = nn.Linear(input_dim, output_dim)
+        self.sigmoid = nn.Sigmoid()
+        
+    def forward(self, x):
+        x = self.linear(x)
+        x = self.sigmoid(x)
+        return x
+    
+    def class_probabilities(self, x):
+        with torch.no_grad():
+            x = self.forward(x)
+            class_probs = torch.cat((1-x, x), dim = 1)
+        return class_probs.reshape(-1,2)
+    
+    def predict(self, x):
+        with torch.no_grad():
+            x = self.forward(x)
+            predicted_class = x.detach().round()
+        return predicted_class.reshape(-1,1)
+
+
+# Insert decision tree model here
+
+
+# Insert random forest model here
+
+
+# Insert MLP model here
+
+
+# (Written by Edvin, edited by Tomas & Agnes)
+class GCN(torch.nn.Module):
+    def __init__(self, input_dim, hidden_dim, output_dim, num_layers, dropout):
+        super(GCN, self).__init__()
+        self.output_dim = output_dim
+        convs = [GCNConv(input_dim, hidden_dim)] + [GCNConv(hidden_dim, hidden_dim) for _ in range(num_layers-2)] + [GCNConv(hidden_dim, output_dim)]
+        self.convs = torch.nn.ModuleList(convs)
+        self.bns = torch.nn.ModuleList([torch.nn.BatchNorm1d(hidden_dim) for _ in range(num_layers-1)])
+        self.dropout = dropout
+        self.softmax = torch.nn.Softmax(dim=1)
+
+    def reset_parameters(self):
+        for conv in self.convs:
+            conv.reset_parameters()
+        for bn in self.bns:
+            bn.reset_parameters()
+
+    def forward(self, data):
+        x, edge_index = data.x, data.edge_index
+        for i, layer in enumerate(self.convs):
+          x = layer(x, edge_index)
+          if i < len(self.convs)-1:
+            x = self.bns[i](x)
+            x = F.relu(x)
+            x = F.dropout(x, p=self.dropout, training=self.training)
+            
+        #Note: When using CrossEntropyLoss, the softmax function is included in the loss function
+        #out = self.softmax(x)
+        out = x
+            
+        return out
+
+
+# (Written by Edvin)
+class GraphSAGE(torch.nn.Module):
+    def __init__(self, input_dim, hidden_dim, output_dim, dropout=0.2):
+        super().__init__()
+        self.dropout = dropout
+        self.node_emb = Linear(input_dim, hidden_dim)
+        self.conv1 = SAGEConv(hidden_dim, hidden_dim)
+        self.conv2 = SAGEConv(hidden_dim, hidden_dim)
+        self.conv3 = SAGEConv(hidden_dim, hidden_dim)
+        self.bn1 = BatchNorm(hidden_dim)
+        self.bn2 = BatchNorm(hidden_dim)
+        self.bn3 = BatchNorm(hidden_dim)
+        self.classifier = Linear(hidden_dim, output_dim)
+    
+    def forward(self, data):
+        x = self.node_emb(data.x)
+        
+        x = self.conv1(x, data.edge_index)
+        x = self.bn1(x)
+        x = F.relu(x)
+        x = F.dropout(x, p=self.dropout)
+        
+        x = self.conv2(x, data.edge_index)
+        x = self.bn2(x)
+        x = F.relu(x)
+        x = F.dropout(x, p=self.dropout)
+        
+        x = self.conv3(x, data.edge_index)
+        x = self.bn3(x)
+        x = F.relu(x)
+        x = F.dropout(x, p=self.dropout)
+        
+        x = self.classifier(x)
+        return torch.softmax(x, dim=-1)
+
+
+# (Written by Tomas & Agnes)
+class GAT(torch.nn.Module):
+    def __init__(self, in_channels, hidden_channels, out_channels, num_heads, dropout=0.3):
+        super(GAT, self).__init__()
+        self.dropout = dropout
+        self.conv1 = GATConv(in_channels, hidden_channels, heads=num_heads, dropout=dropout)
+        self.conv2 = GATConv(hidden_channels * num_heads, hidden_channels, heads=num_heads, dropout=dropout)
+        self.conv3 = GATConv(hidden_channels * num_heads, out_channels, heads=1, concat = False, dropout=dropout)
+
+    def forward(self, x, edge_index):
+        x, attention_weights1 = self.conv1(x, edge_index, return_attention_weights=True)
+        x = F.elu(x)
+        x = F.dropout(x, p=self.dropout, training=self.training)
+        x, attention_weights2 = self.conv2(x, edge_index, return_attention_weights=True)
+        x = F.elu(x)
+        x = F.dropout(x, p=self.dropout, training=self.training)
+        x, attention_weights3 = self.conv3(x, edge_index, return_attention_weights=True)
+        
+        #Note: When using CrossEntropyLoss, the softmax function is included in the loss function
+        #out = self.softmax(x)
+        return x, attention_weights1, attention_weights2, attention_weights3
