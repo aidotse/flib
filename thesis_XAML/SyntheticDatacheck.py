d612203163e60567b51f35f59eb94d044292f2a2
agnesrosendal <agnesrosendal5@gmail.com>
2024-05-25 10:21:25 +0000

    h

--- thesis_XAML/SyntheticDatacheck.py
+++ thesis_XAML/SyntheticDatacheck.py
@@ -6,104 +6,215 @@ import seaborn as sns
 import os
 import data
 import scipy.stats as stats
+import torch
 
 class Datacheck():
     
     # init
-    def __init__(self, dataset:str, importance):
+    def __init__(self, dataset:str, importance, aggregated = True,attention_weights=False):
         self.dataset = dataset
         self.load_data(dataset)
-        if importance!='LLR':
-            self.importance_train, self.importance_test = self.build_importance(self.dataset, usingTrain=True, importance=importance)
+        self.attention_weights = attention_weights
+        self.importance_test = self.build_importance( importance=importance)
         # self.importance_train, self.importance_test = self.build_importance(self.dataset, usingTrain=True, importance=importance)
         self.normal_train
         self.normal_test
+        # self.normal_val
         self.withoutSAR_train
         self.withoutSAR_test
+        self.cols
+        if importance=='llr':
+            self.llr_before
+            self.llr_after
+        # self.withoutSAR_val
+        self.diff_test_final
 
     def load_data(self,dataset):
         DATASET=dataset
 
-        self.normal_train=pd.read_csv(f'data2/{DATASET}/bank/train/nodes.csv')
-        self.normal_test=pd.read_csv(f'data2/{DATASET}/bank/test/nodes.csv')
-        # self.normal_val=pd.read_csv(f'data/{DATASET}/bank/val/nodes.csv')
-        self.withoutSAR_train=pd.read_csv(f'data2/{DATASET}_withoutSAR/bank/train/nodes.csv')
-        self.withoutSAR_test=pd.read_csv(f'data2/{DATASET}_withoutSAR/bank/test/nodes.csv')
-        # self.withoutSAR_val=pd.read_csv(f'data/{DATASET}_withoutSAR/bank/val/nodes.csv')
+        self.normal_train=pd.read_csv(f'ultra_final_data/{DATASET}/bank/train/nodes.csv')
+        self.normal_test=pd.read_csv(f'ultra_final_data/{DATASET}/bank/test/nodes.csv')
+        # self.normal_val=pd.read_csv(f'final_data/{DATASET}/bank/val/nodes.csv')
+        self.withoutSAR_train=pd.read_csv(f'ultra_final_data/{DATASET}_withoutSAR/bank/train/nodes.csv')
+        self.withoutSAR_test=pd.read_csv(f'ultra_final_data/{DATASET}_withoutSAR/bank/test/nodes.csv')
+        # self.withoutSAR_val=pd.read_csv(f'final_data/{DATASET}_withoutSAR/bank/val/nodes.csv')
 
     
+    
+    def calculate_difference(self,normal,withoutSAR):
+
+                
+        diff=pd.merge(normal,withoutSAR,on='account',suffixes=('_1','_2'))
+        diff.fillna(0,inplace=True) #If an account only exists in one of the dataframes, fill the missing values with 0
         
-    def build_importance(self, dataset, usingTrain=True, importance='std'):
+        # # Reorder diff_test based on the index of n_test
+        # diff = diff.set_index('account').loc[normal.set_index('account').index].reset_index()
+
+        for col in self.cols: 
+            diff[col]=diff[col+'_1']-diff[col+'_2']
+            
+        diff=diff.drop(diff.columns[diff.columns.str.endswith('_1')], axis=1)
+        diff=diff.drop(diff.columns[diff.columns.str.endswith('_2')], axis=1)
+        
+        return diff
+    
+    
+    
+    def aggregate_by_attention(self, data):
+        
+        edges = self.attention_weights[0]
+        weights = self.attention_weights[1]
+
+        edges = pd.DataFrame(edges.cpu().detach().numpy())
+        weights = torch.mean(weights, dim=1).cpu().detach().numpy()
+
+        # Initialize an empty DataFrame for importance_test
+        importance_tmp = np.zeros((data.shape[0], len(self.cols)))
+
+        # Convert DataFrame to numpy array for faster processing
+        n_test_values = data[self.cols].values
+
+        for idx in range(data.shape[0]):
+            conn_edges = edges.T[edges.T[1] == idx]
+            conn_nodes = conn_edges[0].values
+            edge_index = conn_edges.index.values
+            connected_weights = weights[edge_index]
+
+            # Iterate over columns and compute weighted sums separately
+            for col_idx, col in enumerate(self.cols):
+                weighted_sum = np.dot(n_test_values[conn_nodes, col_idx], connected_weights)
+                importance_tmp[idx, col_idx] = weighted_sum
+
+            if idx>=10:
+                return pd.DataFrame(importance_tmp, columns=self.cols)
+
+        # return pd.DataFrame(importance_tmp, columns=self.cols)
+
+
+        
+    def build_importance(self, importance='std', aggregated=True):
+        
+        ''' Possible importances are std, llr, and att
+        std= difference divided by std
+        llr= log likelihood ratio before - after
+        '''
         #Importance can be diff_norm, std, or LLR
         
         n_train=pd.DataFrame(self.normal_train)
         n_test=pd.DataFrame(self.normal_test)
         w_train=pd.DataFrame(self.withoutSAR_train)
         w_test=pd.DataFrame(self.withoutSAR_test)
+        
+         
+        cols=n_test.columns
+        cols=cols[2:-1]
+        self.cols=cols
 
-        cols=n_train.columns
+        if aggregated: 
+            n_account=n_test['account']
+            w_account=w_test['account']
+            
+            agg_normal_train=self.aggregate_by_attention(n_train)
+            agg_normal_test=self.aggregate_by_attention(n_test)
+            agg_normal_test['account']=n_account
 
-        #Remove 'account' and 'bank'
-        cols=cols[2:]
+            agg_withoutSAR=self.aggregate_by_attention(w_test)
+            agg_withoutSAR['account']=w_account
+            
+                        
+            diff_test=self.calculate_difference(agg_normal_test,agg_withoutSAR)
+            
+        else:
 
-        diff_train = pd.merge(n_train,w_train, on='account', suffixes=('_1','_2'))
-        diff_test = pd.merge(n_test,w_test,on='account', suffixes=('_1','_2'))
-        # print('diff_test',diff_test.head())
-        for col in cols:
-            diff_train[col] = diff_train[col+'_1'] - diff_train[col+'_2']
-            diff_test[col] = diff_test[col+'_1'] - diff_test[col+'_2']
-
-        diff_train = diff_train.drop(diff_train.columns[diff_train.columns.str.endswith('_1')], axis=1)
-        diff_train = diff_train.drop(diff_train.columns[diff_train.columns.str.endswith('_2')], axis=1)
-        diff_test= diff_test.drop(diff_test.columns[diff_test.columns.str.endswith('_1')], axis=1)
-        diff_test= diff_test.drop(diff_test.columns[diff_test.columns.str.endswith('_2')], axis=1)
-
-        #save account order 
-        accounts_train=diff_train['account']
+            diff_test=self.calculate_difference(n_test,w_test)
         accounts_test=diff_test['account']
 
-        # if (all(diff_train['account']!=diff_te['account'])):
-        #     print('Error: accounts are not the same')
-        #     return
         
-        diff_train_final=diff_train.drop(['account','is_sar'], axis=1)
-        diff_test_final=diff_test.drop(['account','is_sar'], axis=1)
-        #Calculate importance using the mean and std
+        #save account order 
+        
+        self.diff_test_final=diff_test.drop(['account'], axis=1)
+       
 
+            
+        # if aggregated:
+        #     n_account=n_test['account']
+        #     w_account=w_test['account']
+            
+        #     agg_normal_train=self.aggregate_by_attention(n_train)
+        #     agg_normal_test=self.aggregate_by_attention(n_test)
+        #     agg_normal_test['account']=n_account
+
+        #     agg_withoutSAR=self.aggregate_by_attention(w_test)
+        #     agg_withoutSAR['account']=w_account
+            
+        #     std=agg_normal_train.std(axis=0)
+            
+        #     print(std)
+            
+        #     diff_test=self.calculate_difference(agg_normal_test,agg_withoutSAR)
+            
+        #     self.diff_test_final=diff_test.drop(['account'], axis=1)
+            
+            
+        #     importance_test=abs(self.diff_test_final/std)
+            # importance_test['account']=accounts_test
+         
+            
+        
         if (importance=='std'):
         
-            if (usingTrain):
                 #calculate the std for the normal training set, not the difference
                 # print(self.normal_train.head())
+            if aggregated: 
+                std = agg_normal_train.std(axis=0)
+
+            else:
                 std = self.normal_train.std(axis=0)
-            else: 
-                std = self.normal_test.std(axis=0)
-                
-            importance_train=diff_train_final/std
-            importance_train['account']=accounts_train
 
-            importance_test=diff_test_final/std
-            importance_test['account']=accounts_test
-        
-        elif  (importance=='diff_norm'):
 
-            if (usingTrain):
-                diff_mean=diff_train_final.mean(axis=0)
-                diff_std=diff_train_final.std(axis=0)
-            else: 
-                diff_mean=diff_test_final.mean(axis=0)
-                diff_std=diff_test_final.std(axis=0)
 
-            importance_train=(diff_train_final-diff_mean)/diff_std
-            importance_train['account']=accounts_train
-
-            importance_test=(diff_test_final-diff_mean)/diff_std
+            importance_test=abs(self.diff_test_final/std)
             importance_test['account']=accounts_test
             
-        elif  (importance=='LLR'):
-            #Calculate the log likelihood ratio using the 
-            normal_accts=self.normal_train[self.normal_train['is_sar']==0]
-            sar_accts=self.normal_train[self.normal_train['is_sar']==1]
+            importance_test=importance_test[self.cols[0:-1]]
+            return importance_test
+        
+        
+        # elif  (importance=='diff_norm'):
+
+        #     if (usingTrain):
+        #         diff_mean=diff_train_final.mean(axis=0)
+        #         diff_std=diff_train_final.std(axis=0)
+        #     else: 
+        #         diff_mean=diff_test_final.mean(axis=0)
+        #         diff_std=diff_test_final.std(axis=0)
+
+
+        #     importance_test=(diff_test_final-diff_mean)/diff_std
+        #     importance_test['account']=accounts_test
+            
+        elif (importance=='llr'):
+            
+            
+            
+            if aggregated: 
+                print('agg',agg_normal_test.columns)
+                agg_normal_test['is_sar']=n_test['is_sar']
+                agg_normal_train['is_sar']=n_train['is_sar']
+                agg_withoutSAR['is_sar']=w_test['is_sar']
+                
+                normal_accts=agg_normal_test[agg_normal_test['is_sar']==0]
+                sar_accts=agg_normal_test[agg_normal_test['is_sar']==1]
+                node_features=agg_normal_test
+                # n_train=pd.DataFrame(self.normal_train)
+                
+            else:
+                        
+                normal_accts=self.normal_train[self.normal_train['is_sar']==0]
+                sar_accts=self.normal_train[self.normal_train['is_sar']==1]
+                node_features=self.normal_test
+
+                
             
             normal_mean=normal_accts[cols].mean(axis=0)
             normal_std=normal_accts[cols].std(axis=0)
@@ -111,58 +222,72 @@ class Datacheck():
             sar_mean=sar_accts[cols].mean(axis=0)
             sar_std=sar_accts[cols].std(axis=0)
             
-            #Calculate the LLR for the self.normal_test and self.withoutSAR_test
             
-            llr_values = []
-            for idx, row in self.normal_test.iterrows():
-                llr_row = []
-                for col in cols:
-                    x = row[col]
-                    llr_normal = log_likelihood(x, normal_mean[col], normal_std[col])
-                    llr_sar = log_likelihood(x, sar_mean[col], sar_std[col])
-                    llr_row.append(llr_normal - llr_sar)
-                llr_values.append(llr_row)
-            llr_before=pd.DataFrame(llr_values, columns=cols)
-            llr_values=[]
-            for idx, row in self.withoutSAR_test.iterrows():
-                llr_row = []
-                for col in cols:
-                    x = row[col]
-                    llr_normal = log_likelihood(x, normal_mean[col], normal_std[col])
-                    llr_sar = log_likelihood(x, sar_mean[col], sar_std[col])
-                    llr_row.append(llr_normal - llr_sar)
-                llr_values.append(llr_row)
-            llr_after=pd.DataFrame(llr_values, columns=cols)
+            x=node_features
             
-            # Replace 0 values in llr_after with NaN to avoid division by zero
-            llr_after.replace(0, np.nan, inplace=True)
+            llr_normal = log_likelihood(x, normal_mean, normal_std)
+            llr_sar = log_likelihood(x, sar_mean, sar_std)
+            
+            #fill nan values with 0
+            llr_normal.fillna(0, inplace=True)
+            llr_sar.fillna(0, inplace=True)
+            llr_before=llr_normal-llr_sar
+            
+            accounts_normal=self.normal_test['account'].values
+            llr_before['account']=accounts_normal
+            
+            y=self.withoutSAR_test
+            y=y[cols]
+            llr_normal = log_likelihood(y, normal_mean, normal_std)
+            llr_sar = log_likelihood(y, sar_mean, sar_std)
+                
+                 #fill nan values with 0
+            llr_normal.fillna(0, inplace=True)
+            llr_sar.fillna(0, inplace=True)
+            llr_after=llr_normal-llr_sar
+            accounts_withoutSAR=self.withoutSAR_test['account'].values
+            llr_after['account']=accounts_withoutSAR
+            
+            self.llr_before=llr_before
+            self.llr_after=llr_after
+            importance_test=pd.merge(llr_before,llr_after, on='account', suffixes=('_1', '_2'), how='left')
+            print('importance',importance_test)
+            
+            for col in cols:
+                importance_test[col]=importance_test[col+'_2']-importance_test[col+'_1']
+            #     importance_test[col] = importance_test.apply(lambda row: -row[col+'_1']/row[col+'_2'] if row[col+'_1'] <0 and row[col+'_2']>0 else row[col+'_1']/row[col+'_2'] , axis=1)
+            #     importance_test[col] = importance_test.apply(lambda row: -row[col+'_1']/row[col+'_2'] if row[col+'_1'] >0 and row[col+'_2']>0 and row[col+'_1']>row[col+'_2'] else row[col+'_1']/row[col+'_2'] , axis=1)
+            #     importance_test[col] = importance_test.apply(lambda row: -row[col+'_1']/row[col+'_2'] if row[col+'_1'] <0 and row[col+'_2']<0 and row[col+'_1']>row[col+'_2'] else row[col+'_1']/row[col+'_2'], axis=1)
+            #     importance_test[col] = importance_test.apply(lambda row: 0 if row[col+'_1'] == row[col+'_2'] else row[col+'_1']/row[col+'_2'], axis=1)
+                
+                
+                #here i want to make the importance[col] negative for some instances and positive for others. How to adjust
+                
 
-            # the importance is the llr_before divided by llr_after for each node. If division by 0, then the importance is 0
+       
+            importance_test=importance_test.drop(importance_test.columns[importance_test.columns.str.endswith('_1')], axis=1)
+            importance_test=importance_test.drop(importance_test.columns[importance_test.columns.str.endswith('_2')], axis=1)
             
-            importance_train=llr_before/llr_after
-            importance_train['account']=accounts_train
-            
-            importance_test=llr_before/llr_after
-            importance_test['account']=accounts_test
-            
-            
-            # Replace NaN values resulting from division by zero with 0
-            importance_train.fillna(0, inplace=True)
-            importance_test.fillna(0, inplace=True)
+            importance_test=importance_test.drop(['bank','is_sar','sum'],axis=1)
 
             
             
-        #Reorganize columns in dataframes
-        cols = importance_train.columns.tolist()
-        cols = cols[-1:]+cols[:-1]
-        importance_train=importance_train[cols]
-        importance_test=importance_test[cols]
+
+               
+        
+        
+            
+        # #Reorganize columns in dataframes
+        # cols = importance_test.columns.tolist()
+        # # cols = cols[-1:]+cols[:-1]
+        # importance_train=importance_train[cols]
+        # importance_test=importance_test[cols]
 
         #Add is_SAR to the importance df
-        importance_train['is_sar']=diff_train['is_sar']
-        importance_test['is_sar']=diff_test['is_sar']
+        # importance_train['is_sar']=diff_train['is_sar']
+        # importance_test['is_sar']=diff_test['is_sar']
 
-        return importance_train, importance_test
+        return importance_test
 
 
     def get_sar_accounts(self):
@@ -201,7 +326,7 @@ class Datacheck():
         #Change column name
         feature_importance.columns=['importance']
 
-        #Sort by importance column
+        #Sort by importance columnn
         feature_importance=feature_importance.sort_values(by='importance',ascending=False)
 
         #Add columns with org and without SAR values
@@ -230,6 +355,7 @@ class Datacheck():
                
         node_features=self.normal_test[self.normal_test['account']==account_id]
         x=node_features[cols]
+        print(x)
         # print(x)
         llr_normal = log_likelihood(x, normal_mean, normal_std)
         llr_sar = log_likelihood(x, sar_mean, sar_std)
@@ -248,14 +374,18 @@ class Datacheck():
         llr_sar.fillna(0, inplace=True)
         llr_after=llr_normal-llr_sar
         
+        
+        #if  llr_after is empty 
+        if llr_after.empty:
+            llr_after=llr_before
         #remove is sar
         llr_before=llr_before.drop(['is_sar'],axis=1)
         llr_after=llr_after.drop(['is_sar'],axis=1)
         
         importance=llr_before.T
-        importance.columns=['LLR_before']
-        importance['LLR_after']=llr_after.T
-        importance['importance']=importance['LLR_before']/importance['LLR_after']
+        importance.columns=['llr_before']
+        importance['llr_after']=llr_after.T
+        importance['importance']=importance['llr_before']/importance['llr_after']
         
         importance['importance'].fillna(0, inplace=True)
         #put importance column first
@@ -263,7 +393,25 @@ class Datacheck():
         cols = cols[-1:]+cols[:-1]
         importance=importance[cols]
         
-        importance['importance']=-importance['importance']
+        
+        importance['importance'] = importance.apply(lambda row: -row['importance'] if row['llr_before'] <0 and row['llr_after']>0 else row['importance'], axis=1)
+        importance['importance'] = importance.apply(lambda row: -row['importance'] if row['llr_before'] >0 and row['llr_after']>0 and row['llr_before']>row['llr_after'] else row['importance'], axis=1)
+        importance['importance'] = importance.apply(lambda row: -row['importance'] if row['llr_before'] <0 and row['llr_after']<0 and row['llr_before']>row['llr_after'] else row['importance'], axis=1)
+        importance['importance'] = importance.apply(lambda row: 0 if row['llr_before'] == row['llr_after'] else row['importance'], axis=1)
+
+        # print(importance)
+        importance['org']=self.normal_test[self.normal_test['account']==account_id].drop(['account','is_sar'],axis=1).T
+        
+        #get without sar if existing in dataset
+        if account_id in self.withoutSAR_test['account'].values:
+            importance['without_SAR']=self.withoutSAR_test[self.withoutSAR_test['account']==account_id].drop(['account','is_sar'],axis=1).T
+            importance['difference']=importance['org']-importance['without_SAR']
+        else:
+            importance['without_SAR']=0
+            importance['difference']=0
+        importance['difference']=importance['org']-importance['without_SAR']
+
+        # importance['importance']=-importance['importance']
         importance=importance.sort_values(by='importance',ascending=False)
 
         
