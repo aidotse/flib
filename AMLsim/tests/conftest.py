93d8e73e1a1e3addc04896d9e7887c47f070a14d
johanos1 <oshtman@gmail.com>
2024-08-20 21:35:04 +0000

    normal model tests failed after adding alerts

--- AMLsim/tests/conftest.py
+++ AMLsim/tests/conftest.py
@@ -6,7 +6,7 @@ from scripts.transaction_graph_generator import TransactionGenerator
 
 
 class Transaction_Graph:
-    def __init__(self, config_str):
+    def __init__(self, config_str, clean = False):
         self.config_str = config_str
         
         base_dir = os.path.dirname(__file__)
@@ -21,11 +21,23 @@ class Transaction_Graph:
         self.txg.load_account_list()  # Load account list CSV file and write accounts to nodes in network
         self.txg.load_normal_models() # Load a parameter CSV file for Normal Models
         self.txg.build_normal_models() # Build normal models from the base transaction types
-        self.txg.set_main_acct_candidates() # Identify accounts with large amounts of in and out edges
-        self.txg.load_alert_patterns()  # Load alert patterns CSV file and create AML typology subgraphs
-        self.txg.mark_active_edges() # mark all edges in the normal models as active
+        if not clean:
+            self.txg.set_main_acct_candidates() # Identify accounts with large amounts of in and out edges
+            self.txg.load_alert_patterns()  # Load alert patterns CSV file and create AML typology subgraphs
+            self.txg.mark_active_edges() # mark all edges in the normal models as active
         
-# Define a fixture
+@pytest.fixture
+def small_clean_graph():
+    # Setup: Create and return a dictionary
+    config_str = "parameters/small_test/conf.json"
+    return Transaction_Graph(config_str, clean = True).txg
+
+@pytest.fixture
+def large_clean_graph():
+    # Setup: Create and return a dictionary
+    config_str = "parameters/large_test/conf.json"
+    return Transaction_Graph(config_str, clean=True).txg
+
 @pytest.fixture
 def small_graph():
     # Setup: Create and return a dictionary
