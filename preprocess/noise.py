bcab59cd9abdda820536d8bcb234d3145945c545
TheColdIce <edvin.callisen@gmail.com>
2024-08-21 09:28:30 +0000

    rewrote flip neighbours

--- preprocess/noise.py
+++ preprocess/noise.py
@@ -1,32 +1,47 @@
 import pandas as pd
-import os
 
-def flip_labels(data:pd.DataFrame, labels:list=[0, 1], fracs:list=[0.01, 0.1], seed:int=42):
+def flip_labels(nodes:pd.DataFrame, labels:list=[0, 1], fracs:list=[0.01, 0.1], seed:int=42):
     
-    if 'true_label' not in data.columns:
-        data['true_label'] = data['is_sar']
+    if 'true_label' not in nodes.columns:
+        nodes['true_label'] = nodes['is_sar']
     
     for label, frac in zip(labels, fracs):
-        accounts = data[data['true_label'] == label]['account'].sample(frac=frac, random_state=seed)
-        data.loc[data['account'].isin(accounts), 'is_sar'] = 1 - label
+        accounts_to_flip = nodes[nodes['true_label'] == label]['account'].sample(frac=frac, random_state=seed)
+        nodes.loc[nodes['account'].isin(accounts_to_flip), 'is_sar'] = 1 - label
     
-    return data
+    return nodes
 
 
-def missing_labels(data:pd.DataFrame, labels:list=[0, 1], fracs:list=[0.01, 0.1], seed:int=42):
+def missing_labels(nodes:pd.DataFrame, labels:list=[0, 1], fracs:list=[0.01, 0.1], seed:int=42):
     
-    if 'true_label' not in data.columns:
-        data['true_label'] = data['is_sar']
+    if 'true_label' not in nodes.columns:
+        nodes['true_label'] = nodes['is_sar']
     
     for label, frac in zip(labels, fracs):
-        accounts = data[data['true_label'] == label]['account'].sample(frac=frac, random_state=seed)
-        data.loc[data['account'].isin(accounts), 'is_sar'] = -1
+        accounts_to_miss = nodes[nodes['true_label'] == label]['account'].sample(frac=frac, random_state=seed)
+        nodes.loc[nodes['account'].isin(accounts_to_miss), 'is_sar'] = -1
     
-    return data
+    return nodes
 
 
-def flip_neighbours():
-    return
+def flip_neighbours(nodes:pd.DataFrame, edges:pd.DataFrame, frac:float=0.1, seed:int=42):
+    
+    if 'true_label' not in nodes.columns:
+        nodes['true_label'] = nodes['is_sar']
+    
+    # find all normal accounts that are connected to SAR accounts
+    sar_accounts = set(nodes[nodes['true_label'] == 1]['account'])
+    edges_with_sar = edges[edges['src'].isin(sar_accounts) | edges['dst'].isin(sar_accounts)]
+    normal_accounts_with_edges_to_sar_accounts = set(edges_with_sar['src']).union(set(edges_with_sar['dst'])) - sar_accounts
+    normal_accounts_with_edges_to_sar_accounts = normal_accounts_with_edges_to_sar_accounts.intersection(set(nodes[nodes['true_label'] == 0]['account']))
+    normal_accounts_with_edges_to_sar_accounts = pd.DataFrame(normal_accounts_with_edges_to_sar_accounts, columns=['account'])
+    normal_accounts_with_edges_to_sar_accounts = normal_accounts_with_edges_to_sar_accounts.sort_values(by='account')
+    
+    # flip labels of normal accounts that are connected to SAR accounts
+    accounts_to_flip = normal_accounts_with_edges_to_sar_accounts['account'].sample(frac=frac, random_state=seed)
+    nodes.loc[nodes['account'].isin(accounts_to_flip), 'is_sar'] = 1 - nodes.loc[nodes['account'].isin(accounts_to_flip), 'true_label']
+    
+    return nodes, edges
 
 
 def topology_noise():
@@ -36,18 +51,31 @@ def topology_noise():
 def main():
     
     # create dummy data
-    data = pd.DataFrame({
+    nodes = pd.DataFrame({
         'account': ['a100', 'a101', 'a102', 'a103', 'a104', 'a105', 'a106', 'a107', 'a108', 'a109'], 
         'is_sar':[0, 0, 1, 0, 0, 0, 1, 1, 0, 1]
     })
     
     # test functions
     
-    # data_flipped = flip_labels(data=data, labels=[0, 1], fracs=[0.17, 0.25], seed=42)
-    # print(data_flipped)
+    # nodes_flipped_labels = flip_labels(nodes=nodes, labels=[0, 1], fracs=[0.17, 0.25], seed=42)
+    # print(nodes_flipped_labels)
     
-    data_missing = missing_labels(data=data, labels=[0, 1], fracs=[0.17, 0.25], seed=42)
-    print(data_missing)
+    # nodes_missing_labels = missing_labels(nodes=nodes, labels=[0, 1], fracs=[0.17, 0.25], seed=42)
+    # print(nodes_missing_labels)
+    
+    # create dummy data
+    nodes = pd.DataFrame({
+        'account': ['a100', 'a101', 'a102', 'a103', 'a104'], 
+        'is_sar':[1, 0, 0, 0, 0]
+    })
+    edges = pd.DataFrame({
+        'src': ['a100', 'a100', 'a100', 'a100'], 
+        'dst': ['a101', 'a102', 'a103', 'a104'],
+    })
+    
+    nodes_flipped_neighbours, edges = flip_neighbours(nodes=nodes, edges=edges, frac=0.5, seed=42)
+    print(nodes_flipped_neighbours)
 
     
 
