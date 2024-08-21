ece8a7b617dfa6e767203528317d408203398ce1
TheColdIce <edvin.callisen@gmail.com>
2024-08-21 16:02:47 +0000

    rewrote topology_noise

--- preprocess/noise.py
+++ preprocess/noise.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from typing import Optional, Union, List
 
 def flip_labels(nodes:pd.DataFrame, labels:list=[0, 1], fracs:list=[0.01, 0.1], seed:int=42):
     
@@ -44,8 +45,29 @@ def flip_neighbours(nodes:pd.DataFrame, edges:pd.DataFrame, frac:float=0.1, seed
     return nodes, edges
 
 
-def topology_noise():
-    return
+def topology_noise(nodes:pd.DataFrame, alert_members:pd.DataFrame, topologies:list=['fan_in', 'fan_out', 'stack'], fracs:Optional[Union[float, List[float]]]=None, seed:int=42):
+    
+    # TODO: remove alert_members, future updates should ensure that nodes has the necessary information to find the topologies
+    
+    if 'true_label' not in nodes.columns:
+        nodes['true_label'] = nodes['is_sar']
+    
+    if isinstance(fracs, list):
+        assert len(fracs) == len(topologies), 'fracs should have the same length as topologies'
+    elif isinstance(fracs, float):
+        fracs = [fracs] * len(topologies)
+    elif fracs is None:
+        fracs = [0.5] * len(topologies)
+    
+    node_sar = nodes[nodes['is_sar'] == 1][['account','is_sar']]
+    alert_members = alert_members[['reason','accountID']].rename(columns={'accountID':'account'})
+    alert_members = alert_members[alert_members['account'].isin(node_sar['account'])]
+    
+    for topology, frac in zip(topologies, fracs):
+        accounts_to_flip = alert_members[alert_members['reason'] == topology]['account'].sample(frac=frac, random_state=seed)
+        nodes.loc[nodes['account'].isin(accounts_to_flip), 'is_sar'] = 0
+    
+    return nodes
 
 
 def main():
@@ -55,6 +77,10 @@ def main():
         'account': ['a100', 'a101', 'a102', 'a103', 'a104', 'a105', 'a106', 'a107', 'a108', 'a109'], 
         'is_sar':[0, 0, 1, 0, 0, 0, 1, 1, 0, 1]
     })
+    alert_members = pd.DataFrame({
+        'reason': ['fan_in', 'fan_in', 'stack', 'stack'], 
+        'accountID': ['a102', 'a106', 'a107', 'a109']
+    })
     
     # test functions
     
@@ -63,6 +89,9 @@ def main():
     
     # nodes_missing_labels = missing_labels(nodes=nodes, labels=[0, 1], fracs=[0.17, 0.25], seed=42)
     # print(nodes_missing_labels)
+    
+    nodes_flipped_topologies = topology_noise(nodes=nodes, alert_members=alert_members, topologies=['fan_in', 'fan_out', 'stack'], fracs=[1.0, 0.2, 0.5], seed=42)
+    print(nodes_flipped_topologies)
     
     # create dummy data
     nodes = pd.DataFrame({
@@ -74,8 +103,8 @@ def main():
         'dst': ['a101', 'a102', 'a103', 'a104'],
     })
     
-    nodes_flipped_neighbours, edges = flip_neighbours(nodes=nodes, edges=edges, frac=0.5, seed=42)
-    print(nodes_flipped_neighbours)
+    #nodes_flipped_neighbours, edges = flip_neighbours(nodes=nodes, edges=edges, frac=0.5, seed=42)
+    #print(nodes_flipped_neighbours)
 
     
 
